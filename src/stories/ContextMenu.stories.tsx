a1f0acb83033b421da5e601a281a4930e089dbb8
amy-sijinc-chen <chenamy@microsoft.com>
2026-03-27 14:59:37 -0700

    fix: restore React binding in context menu story

--- src/stories/ContextMenu.stories.tsx
+++ src/stories/ContextMenu.stories.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import "../../overrides/vscode-button-overrides.css";
 import {
