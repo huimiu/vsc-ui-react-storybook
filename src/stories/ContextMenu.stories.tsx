47d3cb61f9e7ccb125d640ec5514bc7559546544
amy-sijinc-chen <chenamy@microsoft.com>
2026-03-27 15:20:53 -0700

    Revert "fix: restore React binding in context menu story"
    This reverts commit d1b8492e32bb31fa684ddb2c8fb518e269fcb092.

--- src/stories/ContextMenu.stories.tsx
+++ src/stories/ContextMenu.stories.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import "../../overrides/vscode-button-overrides.css";
 import {
