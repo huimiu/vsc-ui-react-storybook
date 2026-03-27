d4dff4a5f757925125ee1fedc739f337bd6a820a
amy-sijinc-chen <chenamy@microsoft.com>
2026-03-27 15:20:53 -0700

    Revert "fix: restore React binding in Storybook stories"
    This reverts commit dd9b0cd1afdb1a868e982a0423a1c3c385e3d6c8.

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Input } from "@fluentui/react-input";
 import { SearchRegular } from "@fluentui/react-icons";
